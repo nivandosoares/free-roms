446402b0ad207c0e032856622f8b4577290d13b5
Nivando <sidvandoni@gmail.com>
2022-01-15 15:13:37 -0300

    set mongoose connection

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,3 +1,6 @@
+require('../models/database');
+const Category = require('../models/Category');
+
 /**
  * GET /
  * Homepage
@@ -7,3 +10,47 @@ exports.homepage = async(req, res) => {
 
     res.render('index', {title: 'Node games - home'});
 }
+
+/**
+ * Dummy data
+ */
+
+
+
+
+
+
+async function insertDummyCategoryData() {
+    
+    try {
+        await Category.insertMany([
+    {
+        "name": "Metal Gear",
+        "image": "metal-gear.jpg"
+    },
+    {
+        "name": "doom",
+        "image": "doom.jpg"
+    },
+    {
+        "name": "Gran Turismo",
+        "image": "gran-turismo.jpg"
+    },
+    {
+        "name": "Sonic the hedgehog",
+        "image": "sonic.jpg"
+    },
+    {
+        "name": "GTA Vice City Stories",
+        "image": "gta-vcs.jpg"
+    },
+    {
+        "name": "Metal Gear 2: Solid Snake",
+        "image": "metal-gear2.jpg"
+    },
+]); 
+    } catch (error) {
+        console.log(error);
+    }
+}
+insertDummyCategoryData();
